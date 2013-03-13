8da993e83f3f38455024cc7bede45fb1670e0383
kevin <jikeruanjian@126.com>
2013-03-13 09:37:29 +0800

    update
    update

--- src/com/kevin/addressBook/bll/XmlOptionsImp.java
+++ src/com/kevin/addressBook/bll/XmlOptionsImp.java
@@ -3,6 +3,7 @@ package com.kevin.addressBook.bll;
 import java.util.List;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 
 import com.kevin.addressBook.model.AddressInfo;
 
@@ -20,6 +21,12 @@ public class XmlOptionsImp implements IXmlOptions {
 	public static XmlOptionsImp getInstance(String path) {
 		if (instance == null) {
 			XmlOptionsImp.path = path;
+			try { //这里的方式还需要该动
+				XmlOptionsImp.doc = XmlUtility.getDocument(XmlOptionsImp.path);
+			} catch (DocumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			instance = new XmlOptionsImp();
 		}
 		return instance;
